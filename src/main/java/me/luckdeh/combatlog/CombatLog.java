8db43d86edbe881fa53d04493bbc085835f76870
luckdeh <luckdehaaaart@gmail.com>
2024-05-19 23:45:03 +0200

    Initial commit

---
+++ src/main/java/me/luckdeh/combatlog/CombatLog.java
@@ -0,0 +1,17 @@
+package me.luckdeh.combatlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatLog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
