956ad65b67f12fdc2e97d48f3a0e891cf2f57d04
luckdeh <luckdehaaaart@gmail.com>
2024-05-22 15:54:23 +0200

    Started on PlayerDeath

--- src/main/java/me/luckdeh/combatlog/Listener/PlayerDeath.java
+++ src/main/java/me/luckdeh/combatlog/Listener/PlayerDeath.java
@@ -1,4 +1,21 @@
 package me.luckdeh.combatlog.Listener;
 
-public class PlayerDeath {
+import me.luckdeh.combatlog.Handler.TimerHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.UUID;
+
+public class PlayerDeath implements Listener {
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player player = e.getPlayer();
+        UUID playerUUID = player.getUniqueId();
+        TimerHandler timerHandler = TimerHandler.getInstance();
+
+        //Will add the function later
+    }
 }
