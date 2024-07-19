6727312ffb8d01cafa295da04b67a19614a90022
Navoei <SuperNavoeiMC@gmail.com>
2024-07-19 10:16:29 -0500

    Updated Some Stuff

--- src/main/java/me/luckdeh/combatlog/Listener/PlayerDamage.java
+++ src/main/java/me/luckdeh/combatlog/Listener/PlayerDamage.java
@@ -2,9 +2,12 @@ package me.luckdeh.combatlog.Listener;
 
 import me.luckdeh.combatlog.CombatLog;
 import me.luckdeh.combatlog.Handler.TimerHandler;
+import org.bukkit.GameMode;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
@@ -28,6 +31,7 @@ public class PlayerDamage implements Listener {
             attacker = (Player) e.getDamager();
         } else if (e.getDamager() instanceof Projectile projectile) {
             if(projectile.getShooter() instanceof Player){
+                if (projectile.getType().equals(EntityType.SNOWBALL) || projectile.getType().equals(EntityType.EGG)) return;
                 attacker = (Player) projectile.getShooter();
             } else {
                 return;
@@ -37,6 +41,10 @@ public class PlayerDamage implements Listener {
         }
 
         if (attacker.hasPermission("combatlog.bypass") || defender.hasPermission("combatlog.bypass") || attacker.equals(defender)) return;
+
+        if (!attacker.hasPermission("combatlog.tagcreative") && attacker.getGameMode().equals(GameMode.CREATIVE)) {
+            return;
+        }
 
         //Combat Time
         double combatTime = plugin.getConfig().getDouble("combat-time", 30); // Default to 30 if not set
--- src/main/java/me/luckdeh/combatlog/utils/EntityNPC.java
+++ src/main/java/me/luckdeh/combatlog/utils/EntityNPC.java
@@ -5,8 +5,11 @@ import me.luckdeh.combatlog.CombatLog;
 import me.luckdeh.combatlog.Language.Lang;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.*;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 import java.math.RoundingMode;
@@ -33,7 +36,7 @@ public class EntityNPC {
         UUID playerUUID = player.getUniqueId();
 
         EntityType entityType = entityType();
-        Entity entity = location.getWorld().spawnEntity(location, entityType);
+        Entity entity = location.getWorld().spawnEntity(location, entityType, CreatureSpawnEvent.SpawnReason.CUSTOM);
 
         // Set custom name for the entity
         Component customNameComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(Lang.NPC_NAMETAG.toString());
